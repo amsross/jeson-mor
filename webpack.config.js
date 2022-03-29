c5b7c52ac329bf83b71aba8ea91c382ba9c81c47
Matt Ross <amsross@gmail.com>
2022-03-29 11:57:01 -0400

    chore(build): configure build for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const outputDir = path.join(__dirname, "build/");
+const outputDir = path.join(__dirname, "dist/");
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.BRANCH === "main"
+  || process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: "./lib/es6/src/Index.bs.js",
